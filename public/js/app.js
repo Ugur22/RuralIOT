28121f7ebb196aed45370be485c4d14584ee7877
ugur22 <ugurertas9@gmail.com>
2018-05-18 15:32:01 +0200

    add different icons google maps

--- public/js/app.js
+++ public/js/app.js
@@ -14,7 +14,8 @@ var measurementsRef = firebase.database().ref('measurements');
 
 var measurelabels = [];
 var gatewayIds = [];
-
+var imageUrl;
+var turbidity
 
 measurementsRef.on("value", function (snapshot) {
   measurementsLength = snapshot.numChildren();
@@ -47,10 +48,9 @@ function getData(data) {
       ],
     });
     var turbidity = measurements[k].payload_fields.turbidity.toFixed(2);
+
     measurelabels.push(turbidity.toString());
   };
-
-
 
 
 
@@ -67,24 +67,23 @@ function getData(data) {
 var markers = [];
 var map;
 var labelIndex = 0;
-console.log(measurelabels);
 
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 8,
-    center: { lat: 51.8742258, lng: 4.4807875 }
+    zoom: 14,
+    center: { lat: 51.91194, lng: 4.48538 }
   });
 
 }
 
 
-function addMarkerWithTimeout(position, timeout) {
 
+function addMarkerWithTimeout(position, timeout) {
   var markerIcon = {
-    url: 'http://www.clker.com/cliparts/R/y/b/e/w/9/blue-rain-drop-hi.png',
-    scaledSize: new google.maps.Size(50, 50),
+    url: '',
+    scaledSize: new google.maps.Size(40, 40),
     origin: new google.maps.Point(0, 0),
-    labelOrigin: new google.maps.Point(25,33)
+    labelOrigin: new google.maps.Point(50, 33)
   };
 
   window.setTimeout(function () {
@@ -93,21 +92,33 @@ function addMarkerWithTimeout(position, timeout) {
       label: {
         text: measurelabels[labelIndex++ % measurelabels.length],
         color: "#000",
-        fontSize: "12px",
-        fontWeight: "bold"
+        fontSize: "18px",
+        fontWeight: "900"
       },
       icon: markerIcon,
       map: map,
       animation: google.maps.Animation.DROP
     }));
-  }, timeout);
-}
+    for (var i = 0; i < markers.length; i++) {  
+      if (markers[i].label.text < 4) {
+        markers[i].icon.url = 'http://worms2d.info/images/9/9a/Icon_Worm_128x128.png';
+      } else {
+        markers[i].icon.url = 'http://www.clker.com/cliparts/R/y/b/e/w/9/blue-rain-drop-hi.png';
+        markers[i].icon.labelOrigin = new google.maps.Point(20, 20)
+        markers[i].label.fontSize = '15px';
+      }
+    }
 
+  }, timeout);
+
+
+}
 
 
 function clearMarkers() {
   for (var i = 0; i < markers.length; i++) {
     markers[i].setMap(null);
+
   }
   markers = [];
 }
