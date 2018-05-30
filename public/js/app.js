f2c0caf485603ae2107dff8298326a08d834942e
ugur22 <ugurertas9@gmail.com>
2018-05-31 01:21:28 +0200

    clusters on google maps working now without average of an area

--- public/js/app.js
+++ public/js/app.js
@@ -3,37 +3,44 @@ var imageUrl;
 var imageFontSize;
 var labelOriginValue;
 var locations = [];
+var markers = [];
+var infowindow;
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCxvjKhkhrrvHQ1YH4Ob9I80SZ-kXrqI-s",
+  authDomain: "ruralit-1620a.firebaseapp.com",
+  databaseURL: "https://ruralit-1620a.firebaseio.com",
+  projectId: "ruralit-1620a",
+  storageBucket: "ruralit-1620a.appspot.com",
+  messagingSenderId: "687154250117"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+}
 
 function initialize() {
-  var infowindow = new google.maps.InfoWindow();
+
   var map = new google.maps.Map(
     document.getElementById("map"), {
       center: { lat: 51.91194, lng: 4.48538 },
-      zoom: 12,
+      zoom: 10,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     });
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCxvjKhkhrrvHQ1YH4Ob9I80SZ-kXrqI-s",
-    authDomain: "ruralit-1620a.firebaseapp.com",
-    databaseURL: "https://ruralit-1620a.firebaseio.com",
-    projectId: "ruralit-1620a",
-    storageBucket: "ruralit-1620a.appspot.com",
-    messagingSenderId: "687154250117"
-  };
-  if (!firebase.apps.length) {
-    firebase.initializeApp(config);
-  }
 
+   
+
+   map.data.setMap(null);
 
   //Create a node at firebase location to add locations as child keys
   var locationsRef = firebase.database().ref("measurements");
   var bounds = new google.maps.LatLngBounds();
   locationsRef.on('child_added', function (snapshot) {
-
+    infowindow = new google.maps.InfoWindow();
     var data = snapshot.val();
     var turbidity = data.payload_fields.turbidity.toFixed(2).toString();
-
+    var dateMeasurement = data.metadata.time;
+    var latLng = new google.maps.LatLng(data.payload_fields.location.lat, data.payload_fields.location.lng);
 
 
     if (turbidity < 4) {
@@ -53,18 +60,9 @@ function initialize() {
       labelOrigin: labelOriginValue
     };
 
-    locations.push({
-      lat: data.payload_fields.location.lat,
-      lng: data.payload_fields.location.lng
-    });
-
-
-
     var marker = new google.maps.Marker({
-      position: {
-        lat: data.payload_fields.location.lat,
-        lng: data.payload_fields.location.lng
-      },
+      position: latLng,
+      title: "This place has been measured on: " + dateMeasurement.slice(0, 10) + " at: " + dateMeasurement.slice(12, 16),
       label: {
         text: turbidity,
         color: "#000",
@@ -75,17 +73,22 @@ function initialize() {
       map: map
     });
 
-
+    cluster.addMarker(marker);
 
     bounds.extend(marker.getPosition());
-    marker.addListener('click', (function (data) {
+    google.maps.event.addListener(marker,'click', (function (marker) {
       return function (e) {
-        infowindow.setContent(turbidity);
+        infowindow.setContent("This place has been measured on: " + dateMeasurement.slice(0,10) + " at: " + dateMeasurement.slice(12,16));
         infowindow.open(map, this);
       }
-    }(data)));
+    }(marker)));
+    markers.push(marker);
     map.fitBounds(bounds);
   });
+  var cluster = new MarkerClusterer(map, markers, {
+    imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
+  });
+
 
 }
 google.maps.event.addDomListener(window, "load", initialize);
